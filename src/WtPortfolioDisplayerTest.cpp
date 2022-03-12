612a0fe8f4a2478d7e8d7f54d6735e1cfcc450e2
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-03-12 10:48:20 -0500

    Add a table to PortfolioDisplayer

--- src/WtPortfolioDisplayerTest.cpp
+++ src/WtPortfolioDisplayerTest.cpp
@@ -1,22 +1,69 @@
+#include <Wt/Test/WTestEnvironment.h>
+#include <Wt/WEnvironment.h>
+#include <Wt/WApplication.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WTable.h>
+#include <memory>
 #include <string>
 
-class WtPortfolioDisplayer
+class WtPortfolioDisplayer : public Wt::WApplication
 {
 public:
-    WtPortfolioDisplayer(std::string title){}
+    static const std::string Title;
+    static const std::string Subtitle;
+    static const std::string DateColumn;
+    WtPortfolioDisplayer(const Wt::WEnvironment& env);
 
-    std::string title()
-    {
-        return "Title";
-    }
+    Wt::WText* m_subTitle;
+    Wt::WTable* m_currentPortfolioTable;
+private:
 };
+
+WtPortfolioDisplayer::WtPortfolioDisplayer(const Wt::WEnvironment& env)
+    : WApplication(env)
+{
+    setTitle(Title);
+    m_subTitle = 
+        root()->addWidget(std::make_unique<Wt::WText>(Subtitle));
+    m_currentPortfolioTable =
+        root()->addWidget(std::make_unique<Wt::WTable>());
+    m_currentPortfolioTable->elementAt(0, 0)
+              ->addWidget(std::make_unique<Wt::WText>(DateColumn));
+};
+const std::string WtPortfolioDisplayer::Title("Financial Portfolio");
+const std::string WtPortfolioDisplayer::Subtitle("My Portfolio");
+const std::string WtPortfolioDisplayer::DateColumn("Date");
+
 
 #include <gmock/gmock.h>
 using namespace testing;
 
-TEST(AWtPortfolioDisplayer, SetsUpTitleWhenCreated)
+TEST(AWtPortfolioDisplayer, SetsUpTitleOfApplication)
 {
-    WtPortfolioDisplayer displayer("Title");
+    Wt::Test::WTestEnvironment testEnvironment;
+    WtPortfolioDisplayer displayer(testEnvironment);
 
-    ASSERT_THAT(displayer.title(), Eq("Title"));
+    ASSERT_THAT(displayer.title(), Eq(WtPortfolioDisplayer::Title));
+}
+
+TEST(AWtPortfolioDisplayer, HasTextWidget)
+{
+    Wt::Test::WTestEnvironment testEnvironment;
+    WtPortfolioDisplayer displayer(testEnvironment);
+
+    ASSERT_THAT(displayer.m_subTitle->text(), 
+                            Eq(WtPortfolioDisplayer::Subtitle));
+}
+
+TEST(AWtPortfolioDisplayer, HasTableToShowCurrentPortfolio)
+{
+    Wt::Test::WTestEnvironment testEnvironment;
+    WtPortfolioDisplayer displayer(testEnvironment);
+
+    auto firstColumnTextPointer = dynamic_cast<Wt::WText*>(
+            displayer.m_currentPortfolioTable->elementAt(0,0)->widget(0));
+
+    ASSERT_THAT(firstColumnTextPointer->text(), 
+                        Eq(WtPortfolioDisplayer::DateColumn));
 }
